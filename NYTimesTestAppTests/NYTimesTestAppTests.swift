e34fe3fbc5e0b9659fc6261b2489d0292abe2501
Abhishek Chaudhari <jax@Abhisheks-MacBook-Pro.local>
2018-08-17 20:15:36 +0530

    Unit test target added

---
+++ NYTimesTestAppTests/NYTimesTestAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  NYTimesTestAppTests.swift
+//  NYTimesTestAppTests
+//
+//  Created by Abhishek Chaudhari on 17/08/18.
+//  Copyright © 2018 Abhishek Chaudhari. All rights reserved.
+//
+
+import XCTest
+
+class NYTimesTestAppTests: XCTestCase {
+    
+    func testCheck(){
+    }
+
+}
